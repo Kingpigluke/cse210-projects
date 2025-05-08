aff94f02c15dbc38feecf96f71cb7be4193ad88f
Luke Smith <kingpigluke@gmail.com>
2025-05-08 03:09:43 -0600

    I have changed these for homework1 :)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int mysterynumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // We could also use a do-while loop here...
+        while (guess != mysterynumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (mysterynumber > guess)
+            {
+                Console.WriteLine("Gotta go higher!");
+            }
+            else if (mysterynumber < guess)
+            {
+                Console.WriteLine("Gotta go lower!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
