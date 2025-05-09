e058395658b8f946191baa08a17698e1d342ea83
Luke Smith <kingpigluke@gmail.com>
2025-05-08 23:45:41 -0600

    I have changed these for homework once again! :)

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Bounjour tout le monde");
+
+        Circle myCircle = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine($"{myCircle.GetRadius()}");
+        Circle myCircle2 = new Circle();
+        myCircle2.SetRadius(20);
+        Console.WriteLine($"{myCircle2.GetRadius()}");
+
+        Console.WriteLine($"{myCircle.GetArea()}");
+        Console.WriteLine($"{myCircle2.GetArea()}");
+
+        Cylinder myCylinder = new Cylinder();
+        myCylinder.SetCircle(myCircle);
+        myCylinder.SetHeight(10);
+        Console.WriteLine($"{myCylinder.GetVolume()}");
     }
 }
