b9c55d7e1d6dc75d2b319a391e74db784639fa31
Luke Smith <kingpigluke@gmail.com>
2025-07-18 04:29:00 -0600

    final project foundations #1 and #2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,75 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videosList = new List<Video>();
+
+        Video video1 = new Video();
+        video1._title = "My Pet Pig Died";
+        video1._length = "110";
+        video1._author = "Howdy123";
+        Comment video1Comment1 = new Comment();
+        video1Comment1._name = "BobRossLover420";
+        video1Comment1._commentText = "How tragic!";
+        Comment video1Comment2 = new Comment();
+        video1Comment2._name = "Charles Dickens";
+        video1Comment2._commentText = "Sorry for your loss.";
+        Comment video1Comment3 = new Comment();
+        video1Comment3._name = "Sebastian Smith";
+        video1Comment3._commentText = "Now he is bacon, yum! >:)";
+
+        Video video2 = new Video();
+        video2._title = "Goku vs. Vegeta";
+        video2._length = "500";
+        video2._author = "DragonballZLover";
+        Comment video2Comment1 = new Comment();
+        video2Comment1._name = "AuraFarmingGohan";
+        video2Comment1._commentText = "Wow, this fight never ceases to amaze me.";
+        Comment video2Comment2 = new Comment();
+        video2Comment2._name = "Mary Beth";
+        video2Comment2._commentText = "These new fangled cartoons are too violent for my kids! Reported.";
+        Comment video2Comment3 = new Comment();
+        video2Comment3._name = "Toriyama";
+        video2Comment3._commentText = "I remember this part, it was really fun to make it!";
+
+        Video video3 = new Video();
+        video3._title = "Never Lose";
+        video3._length = "195";
+        video3._author = "Andy Johnson";
+        Comment video3Comment1 = new Comment();
+        video3Comment1._name = "Rebecca Williams";
+        video3Comment1._commentText = "Thank you so much for this song! It's amazing!";
+        Comment video3Comment2 = new Comment();
+        video3Comment2._name = "Demarco Sanders";
+        video3Comment2._commentText = "Yo this is fire, keep it up dude!";
+        Comment video3Comment3 = new Comment();
+        video3Comment3._name = "SongCritic1995";
+        video3Comment3._commentText = "This is actually a pretty good song, I rate it 8/10.";
+        Comment video3Comment4 = new Comment();
+        video3Comment4._name = "Lucky Cyan";
+        video3Comment4._commentText = "Love this song!";
+
+        video1._commentList.Add(video1Comment1);
+        video1._commentList.Add(video1Comment2);
+        video1._commentList.Add(video1Comment3);
+
+        video2._commentList.Add(video2Comment1);
+        video2._commentList.Add(video2Comment2);
+        video2._commentList.Add(video2Comment3);
+
+        video3._commentList.Add(video3Comment1);
+        video3._commentList.Add(video3Comment2);
+        video3._commentList.Add(video3Comment3);
+        video3._commentList.Add(video3Comment4);
+
+        videosList.Add(video1);
+        videosList.Add(video2);
+        videosList.Add(video3);
+
+        foreach (Video video in videosList)
+        {
+            video.DisplayVideoDetails();
+            Console.WriteLine();
+        }
+
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -3,7 +3,59 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation2 World!");
-    }
+    {   
+        Console.WriteLine("---------------------------------------------");
+        //order 1 
+        Address address1 = new Address ("123 Sesame Street.", "New York City", "NY", "USA");
+        Customer customer1 = new Customer("Cookie Monster", address1);
+
+        double order1ShippingCost = customer1.GetShippingCost();
+
+        Product order1Product1 = new Product("Extra Large Choclate Chip Cookies", "1354", 1.50, 3);
+        Product order1Product2 = new Product("2% Milk", "1300", 2.00, 2);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(order1Product1);
+        order1.AddProduct(order1Product2);
+        double order1Subtotal = order1.CalculateSubtotal();
+        double order1Total = order1.CalculateTotal();
+
+        //display order 1
+        order1.DisplayShippingLabel(); 
+        Console.WriteLine();
+        order1.DisplayPackingLabel();
+        Console.WriteLine();
+        order1.DisplayCosts(order1Subtotal ,order1ShippingCost, order1Total);
+
+
+        Console.WriteLine("---------------------------------------------");
+
+
+        //order 2
+        Address address2 = new Address("University View", "Rexburg", "Idaho", "USA");
+        Customer customer2 = new Customer("Lucio", address2);
+
+        double order2ShippingCost = customer2.GetShippingCost();
+
+        Product order2Product1 = new Product("Bread Loaf", "2512", 2.00, 2);
+        Product order2Product2 = new Product("Dubstep Remixer", "5200", 5.00, 4);
+        Product order2Product3 = new Product("Papaya", "4010", 2.00, 3);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(order2Product1);
+        order2.AddProduct(order2Product2);
+        order2.AddProduct(order2Product3);
+        double order2Subtotal = order2.CalculateSubtotal();
+        double order2Total = order2.CalculateTotal();
+
+
+        //display order 2
+        order2.DisplayShippingLabel(); 
+        Console.WriteLine();
+        order2.DisplayPackingLabel();
+        Console.WriteLine();
+        order2.DisplayCosts(order2Subtotal, order2ShippingCost, order2Total);
+        Console.WriteLine("---------------------------------------------");
+
+    }   
 }
